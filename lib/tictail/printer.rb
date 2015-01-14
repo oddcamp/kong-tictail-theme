4e613ab7d9032338b73bd78900c803ae5ec03ff5
Ivan Novosad <ivan.novosad@gmail.com>
2015-01-14 10:26:42 +0100

    Printer can print to an arbitrary file

--- lib/tictail/printer.rb
+++ lib/tictail/printer.rb
@@ -1,18 +1,19 @@
 module Tictail
   class Printer
-    def initialize(layout)
+    def initialize(layout, file = "theme.mustache")
       @layout = layout
+      @file = file
     end
 
     def print
       content = @layout.content
 
-      File.open("theme.mustache", "w") do |f|
+      File.open(@file, "w") do |f|
         f.write(content)
       end
       IO.popen('pbcopy', 'w').print content
 
-      puts "Build successful! View your theme in theme.mustache (and it's added to your clipboard for convinient CMD+v into Tictail.com"
+      puts "Build successful! View your theme in #{@file} (and it's added to your clipboard for convinient CMD+v into Tictail.com"
     end
   end
 end
