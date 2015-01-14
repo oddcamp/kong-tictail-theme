72abcb38b3559f87a9413322f3b7e00f85d0d96f
Ivan Novosad <ivan.novosad@gmail.com>
2015-01-14 11:20:51 +0100

    Layout refactored

--- lib/tictail/layout.rb
+++ lib/tictail/layout.rb
@@ -9,9 +9,9 @@ module Tictail
 
     def about_page
       page = File.read("./templates/about_page.mustache")
-      page = page.sub(/\{\{\{price_with_currency\}\}\}/, '{{price_with_currency}}')
 
-      page.sub(/\{\{\{store_description\}\}\}/, '{{store_description}}')
+      page.sub(/\{\{\{price_with_currency\}\}\}/, '{{price_with_currency}}').
+           sub(/\{\{\{store_description\}\}\}/, '{{store_description}}')
     end
 
     def list_page
@@ -22,22 +22,22 @@ module Tictail
 
     def product_page
       page = File.read("./templates/product_page.mustache")
-      page = page.sub(/\{\{\{social_buttons\}\}\}/, '{{social_buttons}}')
-      page = page.sub(/\{\{\{price_with_currency\}\}\}/, '{{price_with_currency}}')
 
-      page.sub(/\{\{\{description\}\}\}/, '{{description}}')
+      page.sub(/\{\{\{social_buttons\}\}\}/, '{{social_buttons}}').
+           sub(/\{\{\{price_with_currency\}\}\}/, '{{price_with_currency}}').
+           sub(/\{\{\{description\}\}\}/, '{{description}}')
     end
 
     def layout
       layout_string = File.read("./templates/layout.mustache")
-      layout_string = layout_string.sub(/\{\{\{search\}\}\}/, '{{search}}')
-      layout_string = layout_string.sub(/\<script id=\"theme-builder\" src=\"\/theme-builder.js\"\>\<\/script\>/, '')
-      layout_string = layout_string.sub(/\<script src=\"\/assets\/dist\/application.min.js\"\>\<\/script\>/, '<script src="//cdn.konginitiative.com/static/assets/dist/application.min.js"></script>')
-      layout_string = layout_string.sub(/\<link href=\"\/assets\/dist\/application\.css\" rel=\"stylesheet\" type=\"text\/css\"\>/, '<link rel="stylesheet" type="text/css" href="//cdn.konginitiative.com/static/assets/dist/application.css">')
-      layout_string = layout_string.sub(/\{\{\> tictail\/misc\}\}/, '')
-      layout_string = layout_string.gsub(/\{\{#has_children\}\}/, '{{#children?}}')
 
-      layout_string.gsub(/\{\{\/has_children\}\}/, '{{/children?}}')
+      layout_string.sub(/\{\{\{search\}\}\}/, '{{search}}').
+                    sub(/\<script id=\"theme-builder\" src=\"\/theme-builder.js\"\>\<\/script\>/, '').
+                    sub(/\<script src=\"\/assets\/dist\/application.min.js\"\>\<\/script\>/, '<script src="//cdn.konginitiative.com/static/assets/dist/application.min.js"></script>').
+                    sub(/\<link href=\"\/assets\/dist\/application\.css\" rel=\"stylesheet\" type=\"text\/css\"\>/, '<link rel="stylesheet" type="text/css" href="//cdn.konginitiative.com/static/assets/dist/application.css">').
+                    sub(/\{\{\> tictail\/misc\}\}/, '').
+                    gsub(/\{\{#has_children\}\}/, '{{#children?}}').
+                    gsub(/\{\{\/has_children\}\}/, '{{/children?}}')
     end
   end
 end
