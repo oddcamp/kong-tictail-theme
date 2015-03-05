2eff3a25ee9e846d2f16ad9620e4d07d629f8f8b
Per Sandström <per@helloper.com>
2015-03-05 11:20:55 +0100

    add more timestamp info

--- lib/tictail/layout.rb
+++ lib/tictail/layout.rb
@@ -42,11 +42,11 @@ module Tictail
 
       layout_string.gsub(/\{\{\{search\}\}\}/, '{{search}}').
                     gsub(/\<script id="theme-builder" src="\/theme-builder.js">\<\/script>/, '').
-                    gsub(/\<script src="\/assets\/dist\/application.min.js">\<\/script>/, "<script src=\"https://kong-cdn.kollegorna.se/assets/dist/application.min.js?#{timestamp}\"></script>").
-                    gsub(/\<link href="\/assets\/dist\/application\.css" rel="stylesheet" type="text\/css">/, '<link rel="stylesheet" type="text/css" href="https://kong-cdn.kollegorna.se/assets/dist/application.css">').
-                    gsub(/\<link href="\/assets\/images/, '<link href="https://kong-cdn.kollegorna.se/assets/images').
+                    gsub(/\<script src="\/assets\/dist\/application.min.js">\<\/script>/, "<script src=\"#{base_url}/assets/dist/application.min.js?#{timestamp}\"></script>").
+                    gsub(/\<link href="\/assets\/dist\/application\.css" rel="stylesheet" type="text\/css">/, "<link rel=\"stylesheet\" type=\"text/css\" href=\"#{base_url}/assets/dist/application.css?#{timestamp}\">").
+                    gsub(/\<link href="\/assets\/images/, "<link href=\"#{base_url}/assets/images").
                     gsub(/kongmixpanel/, '9d0ac2764fd18a0b68a892557923c0ab').
-                    gsub(/\<img src="\/assets/, '<img src="https://kong-cdn.kollegorna.se/assets').
+                    gsub(/\<img src="\/assets/, "<img src=\"#{base_url}/assets").
                     gsub(assets_regex, "\"#{base_url}\\1?#{timestamp}\"").
                     gsub(/\{\{\> tictail\/misc\}\}/, '').
                     gsub(/\{\{#has_children\}\}/, '{{#children?}}').
